256f9bf553b031f6b47f0b505e910fbbe9894926
Gares <rolaescobar@gmail.com>
2020-02-18 12:00:47 -0600

    Create 2-promises.js

---
+++ 2-promises.js
@@ -0,0 +1,18 @@
+function resolverAfter2Seconds(x) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve(x);
+        }, 2000);
+
+    })
+}
+
+
+async function add1(x) {
+    const a = await resolverAfter2Seconds(25)
+    const b = await resolverAfter2Seconds(30)
+    return x + a + b
+}
+
+add1(10).then(v=>{console.log(v)})
+
