8913b6f42ebfb0fbf622bc3cdefb9c74daa11c64
Gares <rolaescobar@gmail.com>
2020-02-18 09:01:52 -0600

    Create 2-expresiones.js

---
+++ 2-expresiones.js
@@ -0,0 +1,16 @@
+
+//Utilizando Map
+var numbers = [1,5,10,15];
+var doubles  = numbers.map(function(x){return x*2;});
+
+console.log(doubles)
+
+var number = [1,4,9]
+var roots = number.map(function(num){
+  
+    return Math.sqrt(num);
+  
+  
+})
+
+console.log(roots)
