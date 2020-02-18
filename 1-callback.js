57819d4ab3dca9bfc56decace9ac09e74753b91c
Gares <rolaescobar@gmail.com>
2020-02-18 10:28:39 -0600

    Create 1-callback.js

---
+++ 1-callback.js
@@ -0,0 +1,21 @@
+
+function printUno() {
+
+    console.log('uno')
+}
+
+function printDos() {
+
+    console.log('dos')
+}
+
+function printTres() {
+
+    console.log('tres')
+}
+
+setTimeout(printUno,1000)
+setTimeout(printDos,0)
+
+
+printTres();
